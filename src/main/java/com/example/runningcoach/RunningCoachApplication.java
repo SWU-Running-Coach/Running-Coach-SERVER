888b7dbd2a62cdbb9d32fbb5d8a9d789b791a447
dlwndms0812 <dlwndms0812@naver.com>
2023-05-17 17:04:25 +0900

    init

---
+++ src/main/java/com/example/runningcoach/RunningCoachApplication.java
@@ -0,0 +1,13 @@
+package com.example.runningcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningCoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/runningcoach/RunningCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runningcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
