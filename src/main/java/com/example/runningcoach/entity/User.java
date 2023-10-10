dd011d4e667c2ad9833d4d132ddc07402e22781f
dlwndms0812 <dlwndms0812@naver.com>
2023-10-10 15:30:19 +0900

    fix: test code 성공을 위한 User 엔티티 수정
    - test 코드 추가 및 수정
    - User Entity의 role 형식 변경 예정

--- src/main/java/com/example/runningcoach/entity/User.java
+++ src/main/java/com/example/runningcoach/entity/User.java
@@ -1,51 +1,77 @@
 package com.example.runningcoach.entity;
 
-import com.example.runningcoach.enums.Role;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
-	@Column(length = 100, nullable = false)
-	private String email;
-	@Column(length = 255, nullable = false)
-	private String password;
-	@Column(length = 45, nullable = false)
-	private String nickname;
-	@Column(length = 200, columnDefinition = "default_url")
-	private String image;
-	@Column(columnDefinition = "1")
-	private int status;
-	@Column(nullable = false)
-	private LocalDateTime createdDate;
-	@Column(nullable = true)
-	private LocalDateTime deletedDate;
-	@Enumerated(EnumType.STRING)
-	@Column(length = 15, columnDefinition = "MEMBER")
-	private Role role;
 
-	@Builder
-	public User(String email, String password, String nickname, String image, int status, LocalDateTime createdDate, LocalDateTime deletedDate, Role role) {
-		this.email = email;
-		this.password = password;
-		this.nickname = nickname;
-		this.image = image;
-		this.status = status;
-		this.createdDate = createdDate;
-		this.deletedDate = deletedDate;
-		this.role = role;
-	}
+	@Column(length = 100)
+	private String email;
+
+	@Column(length = 255)
+	private String password;
+
+	@Column(length = 45)
+	private String nickname;
+
+	@Builder.Default
+	@Column(columnDefinition = "varchar(200) default 'default_url'")
+	private String image = "default_url";
+
+	@Builder.Default
+	@Column(columnDefinition = "int default 1")
+	private int status = 1;
+
+	private LocalDateTime createdDate;
+
+	private LocalDateTime deletedDate;
+
+	// TODO: String 타입이 아닌 Role 타입으로 변경
+	@Builder.Default
+	@Column(columnDefinition = "varchar(15) default 'MEMBER'")
+	private String role = "MEMBER";
+//
+//	@Builder
+//	public User(String email, String password, String nickname, String image, int status, LocalDateTime createdDate, LocalDateTime deletedDate, String role) {
+//		this.email = email;
+//		this.password = password;
+//		this.nickname = nickname;
+//		this.image = image;
+//		this.status = status;
+//		this.createdDate = createdDate;
+//		this.deletedDate = deletedDate;
+//		this.role = role;
+//	}
+//
+//	@Builder
+//	public User(String email, String password, String nickname, LocalDateTime createdDate, LocalDateTime deletedDate) {
+//		this.image = "default_url";
+//		this.status = 1;
+//		this.role = "MEMBER";
+//
+//		this.email = email;
+//		this.password = password;
+//		this.nickname = nickname;
+//		this.createdDate = createdDate;
+//		this.deletedDate = deletedDate;
+//	}
+
 }
--- src/main/java/com/example/runningcoach/repository/UserRepository.java
+++ src/main/java/com/example/runningcoach/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.runningcoach.repository;
 
 import com.example.runningcoach.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	Optional<User> findByEmail(String email);
 }
