dc3d1c7c1219a06b9d26b8fee4c0329c04dc1716
dlwndms0812 <dlwndms0812@naver.com>
2023-10-11 16:53:38 +0900

    feat: Security config 설정

--- src/main/java/com/example/runningcoach/config/SecurityConfig.java
+++ src/main/java/com/example/runningcoach/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.example.runningcoach.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +16,14 @@ public class SecurityConfig  {
 	@Bean
 	public PasswordEncoder PasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf(CsrfConfigurer::disable);
+		httpSecurity.authorizeHttpRequests(authorize->
+			authorize.requestMatchers("/**").permitAll()
+		);
+		return httpSecurity.build();
 	}
 }
