b56b581dd068497ef942219994b1225892915fab
dlwndms0812 <dlwndms0812@naver.com>
2023-10-12 15:22:12 +0900

    feat: 회원가입 Dto valid

--- src/main/java/com/example/runningcoach/controller/UserController.java
+++ src/main/java/com/example/runningcoach/controller/UserController.java
@@ -5,9 +5,14 @@ import com.example.runningcoach.response.BaseResponse;
 import com.example.runningcoach.response.ResponseMessage;
 import com.example.runningcoach.response.StatusCode;
 import com.example.runningcoach.service.UserService;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +27,13 @@ public class UserController {
 
 	//TODO: swagger 등록
 	@GetMapping("/join")
-	public ResponseEntity signUp(@RequestBody SignupRequestDto signupRequestDto) {
-		//TODO: exception 처리
-
+	public ResponseEntity signUp(@RequestBody @Valid SignupRequestDto signupRequestDto, BindingResult bindingResult) {
+		//valid check
+		if (bindingResult.hasErrors()) {
+			return new ResponseEntity(BaseResponse.response(StatusCode.BAD_REQUEST, Objects.requireNonNull(
+				bindingResult.getFieldError()).getDefaultMessage()),
+				HttpStatus.BAD_REQUEST);
+		}
 		userService.SignupUser(signupRequestDto);
 
 		return new ResponseEntity(BaseResponse.response(StatusCode.CREATED, ResponseMessage.SUCCESS),
--- src/main/java/com/example/runningcoach/dto/SignupRequestDto.java
+++ src/main/java/com/example/runningcoach/dto/SignupRequestDto.java
@@ -1,20 +1,31 @@
 package com.example.runningcoach.dto;
 
+import com.example.runningcoach.response.ResponseMessage;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
-    //TODO: 비밀번호 오류
-    //TODO: 빈칸 오류
-    //TODO: 이메일 형식 오류
 
+    //TODO: 이메일이든 비밀번호든 값이 안들어오면 다 똑같은 에러메세지 반환 하는데, 뭐가 안들어왔는지 알려줘야하나 논의하기
     @Schema(description = "이메일")
+    @NotBlank(message = ResponseMessage.EMPTY_VALUE)
+    @Email(message = ResponseMessage.INVALID_EMAIL)
     private String email;
+
+    //TODO: 비밀번호 글자수 제한 논의해보기
     @Schema(description = "비밀번호")
+    @NotBlank(message = ResponseMessage.EMPTY_VALUE)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = ResponseMessage.INVALID_PASSWORD)
     private String password;
+
+    //TODO: 닉네임 글자수 제한 논의해보기
     @Schema(description = "닉네임")
+    @NotBlank(message = ResponseMessage.EMPTY_VALUE)
     private String nickname;
 }
