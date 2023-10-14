1d3bbaad1042b6292c8ad4a0e2c25359b03cfb96
dlwndms0812 <dlwndms0812@naver.com>
2023-10-14 15:23:28 +0900

    feat: 로그인 controller 구현

--- src/main/java/com/example/runningcoach/dto/LoginRequestDto.java
+++ src/main/java/com/example/runningcoach/dto/LoginRequestDto.java
@@ -1,6 +1,8 @@
 package com.example.runningcoach.dto;
 
+import com.example.runningcoach.response.ResponseMessage;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +10,9 @@ import lombok.Setter;
 @Setter
 public class LoginRequestDto {
     @Schema(description = "이메일")
+    @NotBlank(message = ResponseMessage.EMPTY_VALUE)
     private String email;
     @Schema(description = "비밀번호")
+    @NotBlank(message = ResponseMessage.EMPTY_VALUE)
     private String password;
 }
