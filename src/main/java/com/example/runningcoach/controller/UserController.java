7148830388e5e6dbe59e6c9a9e954e73d5a053ea
dlwndms0812 <dlwndms0812@naver.com>
2023-10-16 15:18:38 +0900

    feat: 회원정보 수정 controller 구현

--- src/main/java/com/example/runningcoach/controller/UserController.java
+++ src/main/java/com/example/runningcoach/controller/UserController.java
@@ -3,17 +3,20 @@ package com.example.runningcoach.controller;
 import com.example.runningcoach.dto.LoginRequestDto;
 import com.example.runningcoach.dto.MypageResponseDto;
 import com.example.runningcoach.dto.SignupRequestDto;
+import com.example.runningcoach.dto.UpdateUserRequestDto;
 import com.example.runningcoach.response.BaseResponse;
 import com.example.runningcoach.response.ResponseMessage;
 import com.example.runningcoach.response.StatusCode;
 import com.example.runningcoach.service.UserService;
 import jakarta.validation.Valid;
 import java.util.Objects;
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -64,6 +67,23 @@ public class UserController {
 		MypageResponseDto mypageResponseDto = userService.myPage(email);
 
 		return new ResponseEntity(BaseResponse.response(StatusCode.OK, ResponseMessage.SUCCESS, mypageResponseDto),
+			HttpStatus.OK);
+	}
+
+	//TODO: param 대신 jwt 토큰 설정
+	@PatchMapping("/info/{email}")
+	public ResponseEntity updateUser(@PathVariable String email, @RequestBody @Valid UpdateUserRequestDto updateUserRequestDto,
+		BindingResult bindingResult) {
+		//valid check
+		if (bindingResult.hasErrors()) {
+			return new ResponseEntity(BaseResponse.response(StatusCode.BAD_REQUEST, Objects.requireNonNull(
+				bindingResult.getFieldError()).getDefaultMessage()),
+				HttpStatus.BAD_REQUEST);
+		}
+
+		userService.updateUser(updateUserRequestDto, email);
+
+		return new ResponseEntity(BaseResponse.response(StatusCode.OK, ResponseMessage.SUCCESS),
 			HttpStatus.OK);
 	}
 }
