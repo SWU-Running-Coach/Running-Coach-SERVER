bc1b7ae61f88e3a1e8c0b3e7860c64241965e5f4
dlwndms0812 <dlwndms0812@naver.com>
2023-10-16 16:13:43 +0900

    style: 안쓰는 import 삭제

--- src/main/java/com/example/runningcoach/controller/UserController.java
+++ src/main/java/com/example/runningcoach/controller/UserController.java
@@ -10,7 +10,6 @@ import com.example.runningcoach.response.StatusCode;
 import com.example.runningcoach.service.UserService;
 import jakarta.validation.Valid;
 import java.util.Objects;
-import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
