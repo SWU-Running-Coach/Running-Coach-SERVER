114ed4d37228bc315ee50a4532bae65a3bede700
dlwndms0812 <dlwndms0812@naver.com>
2023-10-21 16:00:20 +0900

    feat: 달리기 자세분석 피드백 entity와repository 구현

---
+++ src/main/java/com/example/runningcoach/entity/Running.java
@@ -0,0 +1,43 @@
+package com.example.runningcoach.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Running {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long runningId;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(length = 255)
+	private String image;
+
+	private int cadence;
+
+	private double legAngle;
+
+	private double uppderBodyAngle;
+
+	private LocalDateTime dateTime;
+
+}
