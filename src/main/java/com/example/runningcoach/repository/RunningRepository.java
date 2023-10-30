bb6319e34c6c9fac752830b5212f98b3b906b2f5
dlwndms0812 <dlwndms0812@naver.com>
2023-10-31 01:18:51 +0900

    feat: running repository에서 email과 datetime으로 조회 구현

--- src/main/java/com/example/runningcoach/repository/RunningRepository.java
+++ src/main/java/com/example/runningcoach/repository/RunningRepository.java
@@ -1,6 +1,7 @@
 package com.example.runningcoach.repository;
 
 import com.example.runningcoach.entity.Running;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface RunningRepository extends JpaRepository<Running, Long> {
 
 	List<Running> findByUserUserId(Long userId);
+	List<Running> findByUserEmailAndDateTime(String email, LocalDateTime dateTime);
 }
