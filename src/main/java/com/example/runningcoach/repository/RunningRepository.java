c1760f472d214258d39f7cf0aa9ffc7e135201f9
dlwndms0812 <dlwndms0812@naver.com>
2023-11-06 13:44:32 +0900

    feat: 원하는 월과 이메일로 조회 메소드 구현

--- src/main/java/com/example/runningcoach/repository/RunningRepository.java
+++ src/main/java/com/example/runningcoach/repository/RunningRepository.java
@@ -2,8 +2,11 @@ package com.example.runningcoach.repository;
 
 import com.example.runningcoach.entity.Running;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +14,9 @@ public interface RunningRepository extends JpaRepository<Running, Long> {
 
 	List<Running> findByUserUserId(Long userId);
 	List<Running> findByUserEmailAndDateTime(String email, LocalDateTime dateTime);
+
+	@Query("SELECT r FROM Running r WHERE EXTRACT(MONTH FROM r.dateTime) = :month AND r.user.email = :email")
+	List<Running> findByDateTimeMonthAndUserEmail(@Param("month") int month, @Param("email")String email);
+
+
 }
